adda2b22af72e33e134f4afba397b808da70c50a
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-05-04 14:09:34 -0600

    navbar and logo implemented

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,17 @@
 import { PropsWithChildren } from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }: PropsWithChildren<{}>){
     return (
         <>
-            
+            <Head>
+                <title>Unparalleled</title>
+            </Head>
+            <Navbar />
+            <main>
+                {children}
+            </main>
         </>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default function Navbar(){
     return (
         <header className="navbar">
-            
+            <img src="/ryan-logo.png" />
+            <ul>
+                <Link href="/profile">
+
+                </Link>
+            </ul>
         </header>
     )
 }
